4bfc793877222b8a80576433329ce363eed6e2f7
Nasuh Emre ATEŞSOY <emreatessoy@gmail.com>
2026-02-09 00:17:09 +0300

    Agent: Habbit tracker app in urun olarak degerlendirip analizini cikartabilirmisin teknik stack ve neler olacak mobil app olarak backend degil tamamen kendi icin db si falan olacak herseyi uctan uca cikartip dev e atamasini yapalim PO agent

---
+++ src/screens/HabitListScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+const HabitListScreen = () => {
+  return (
+    <View>
+      <Text>Habit List Screen</Text>
+      <Button title="Add Habit" onPress={() => {}} />
+    </View>
+  );
+};
+
+export default HabitListScreen;
