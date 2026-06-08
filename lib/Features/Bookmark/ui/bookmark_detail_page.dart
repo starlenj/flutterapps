caa395ad8ec9f775a0afd764da7d4f7a85c8a5ed
nasuh-macbook <nasuh@nasuh-macbook-MacBook-Pro.local>
2026-06-08 23:04:05 +0300

    1 hata kaldı sadece onu fix et

--- lib/Features/Bookmark/ui/bookmark_detail_page.dart
+++ lib/Features/Bookmark/ui/bookmark_detail_page.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp/Features/Bookmark/Model/bookmark_model.dart';
 
 class BookmarkDetailPage extends StatelessWidget {
   final int bookmarkId;
+  final Bookmark bookmark;
 
-  const BookmarkDetailPage({super.key, required this.bookmarkId});
+  const BookmarkDetailPage({super.key, required this.bookmark});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Detail')),
-      body: Center(child: Text('Bookmart #$bookmarkId')),
+      appBar: AppBar(
+        title: const Text('Detail'),
+        actions: [
+          IconButton(onPressed: () => {}, icon: const Icon(Icons.edit)),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                AspectRatio(aspectRatio: 16 / 9, child: bookmark.image),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
